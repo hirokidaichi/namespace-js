d78794c8acd76e7a4baefef90a5599804f0fc48a
Daichi Hiroki <hirokidaichi@gmail.com>
2010-05-04 19:27:07 +0900

    file

---
+++ t/sample.js
@@ -0,0 +1,5 @@
+(function(){
+    return {
+        SampleClass : function(){}
+    };
+})();
---
+++ t/sample2.js
@@ -0,0 +1,8 @@
+
+(function(){
+    
+    return {
+        TCPServer : function(){},
+        TCPClient : function(){}
+    };
+})();
